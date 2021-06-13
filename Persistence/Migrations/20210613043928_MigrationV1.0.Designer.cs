02acec2324c303c6f7a1612f88aac734fdfc116c
Gilgamesh21 <superoncejonas@hotmail.com>
2021-06-12 22:42:58 -0600

    Añadiendo migracion

--- Persistence/Migrations/20210613043928_MigrationV1.0.Designer.cs
+++ Persistence/Migrations/20210613043928_MigrationV1.0.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contexts;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210613043928_MigrationV1.0")]
+    partial class MigrationV10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20210613043928_MigrationV1.0.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class MigrationV10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Edad",
+                table: "Clientes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Edad",
+                table: "Clientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
